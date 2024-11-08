e79d4a30ee14783aca20a9298c21c6482ab2f44e
Yoel <yobelb8@gmail.com>
2024-11-08 09:38:56 +0000

    Añadidos más usuarios

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,13 +1,18 @@
 
 data class Account(val name: String , val image: String)
+data class Publication(val imagePublication: String)
 
 val users = listOf<Account>(
     Account("perez_toledo", "img1.jpeg"),
     Account("iris_piña", "img2.jpeg"),
     Account("tumama123","img3.jpeg"),
-    Account("felipe1923", "img3.jpeg"),
-    Account("ortfrutas", "img3.jpeg"),
-    Account("memes_daily", "img3.jpeg"),
-    Account("jijopureto10", "img3.jpeg"),
+    Account("felipe1923", "img4.jpeg"),
+    Account("ortfrutas", "img5.jpeg"),
+    Account("memes_daily", "img6.jpeg"),
+    Account("jijopureto10", "img7.jpeg"),
+)
+
+val publications = listOf<Publication>(
+    Publication("")
 )
 
